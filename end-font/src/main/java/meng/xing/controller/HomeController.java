fdd3fafd6dfb3204d5f021c4569d19a1cc9a5522
MengStar <641510128@qq.com>
2017-07-13 09:47:42 +0800

    去shiro，还是security好用

--- end-font/src/main/java/meng/xing/controller/HomeController.java
+++ end-font/src/main/java/meng/xing/controller/HomeController.java
@@ -1,7 +1,5 @@
 package meng.xing.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        Subject currentUser = SecurityUtils.getSubject();
-        System.out.println(currentUser.isAuthenticated());
         return "hellow";
     }
 }
