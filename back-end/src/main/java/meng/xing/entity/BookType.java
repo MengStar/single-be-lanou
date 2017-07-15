19c94a44aa87b2c8831b04432d7dbb2999ee58b1
MengStar <641510128@qq.com>
2017-07-15 19:19:11 +0800

    修改错误

--- back-end/src/main/java/meng/xing/entity/BookType.java
+++ back-end/src/main/java/meng/xing/entity/BookType.java
@@ -1,9 +1,8 @@
 package meng.xing.entity;
 
 import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Administrator on 2017/7/14.
@@ -12,4 +11,10 @@ import javax.persistence.Table;
 @Data
 @Table(name = "book_type")
 public class BookType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String type; //类型名
+    private String description;//类型描述
 }
--- back-end/src/main/java/meng/xing/repository/UserRepository.java
+++ back-end/src/main/java/meng/xing/repository/UserRepository.java
@@ -1,27 +1,22 @@
 package meng.xing.repository;
 
 import meng.xing.entity.User;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-
 /**
  * jpa自动实现接口，默认实现了常见的方法，比如save(),findAll()等等
  * 根据方法名自动操作数据库数据
  */
 @Repository
 @RepositoryRestResource(exported = false) //不是rest风格的资源，不能通过url直接获取。必须从controller访问
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
-
     Page<User> findAll(Pageable pageable);
-
     @Transactional
     long deleteByUsername(String username);
 }
