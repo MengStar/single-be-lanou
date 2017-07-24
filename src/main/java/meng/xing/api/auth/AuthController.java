b678bccd066b686cb43b9e4c9b480e4319dbeda3
MengStar <641510128@qq.com>
2017-07-24 11:01:12 +0800

    返回json格式的Token

--- src/main/java/meng/xing/api/auth/AuthController.java
+++ src/main/java/meng/xing/api/auth/AuthController.java
@@ -7,6 +7,7 @@ import meng.xing.service.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,27 +21,31 @@ public class AuthController {
     private AuthService authService;
     @Autowired
     private UserService userService;
+
     /**
      * 登录并返回token
      *
      * @return 成功：token 200;失败：null 200
      */
     @PostMapping("/login")
-    public String login(@RequestBody(required = true) Map<String, Object> map) {
+    public Map<String, Object> login(@RequestBody(required = true) Map<String, Object> map) {
         String username = map.get("username").toString();
         String password = map.get("password").toString();
-        return "{\"token\":\"" + authService.login(username, password) + "\"}";
+        Map<String, Object> retObj = new HashMap<>();
+        retObj.put("token", authService.login(username, password));
+        return retObj;
     }
 
     /**
      * 用户注册
+     *
      * @return 成功：token 200 ;失败：null 200
      */
     @PostMapping("/register")
-    public String register(@RequestBody(required = true) Map<String, Object> map) {
+    public Map<String, Object> register(@RequestBody(required = true) Map<String, Object> map) {
         String username = map.get("username").toString();
         //用户名存在
-        if (userService.findUserByUsername(username) != null){
+        if (userService.findUserByUsername(username) != null) {
             return null;
         }
         String password = map.get("password").toString();
@@ -48,12 +53,14 @@ public class AuthController {
         String phone = map.get("phone").toString();
         String email = map.get("email").toString();
         String address = map.get("address").toString();
-        boolean female = (boolean)map.get("female");
-        int age = (int)map.get("age");
-        User user = new User(username,password,nickName,phone,email,address,female,age);
+        boolean female = (boolean) map.get("female");
+        int age = (int) map.get("age");
+        User user = new User(username, password, nickName, phone, email, address, female, age);
         authService.register(user);
         userService.setUserRoles(username, UserRoleEnum.ROLE_DEFAULT.toString());
-        return "{\"token\":\"" + authService.login(username, password) + "\"}";
+        Map<String, Object> retObj = new HashMap<>();
+        retObj.put("token", authService.login(username, password));
+        return retObj;
     }
 
     /**
